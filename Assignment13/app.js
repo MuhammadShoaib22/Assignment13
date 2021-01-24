803704a1787258a1247324a7ed26bd6dfdb6b636
Muhammad Shoaib <66721620+MuhammadShoaib22@users.noreply.github.com>
2021-01-24 22:55:02 +0500

    Add files via upload

---
+++ Assignment13/app.js
@@ -0,0 +1,31 @@
+// var val=prompt("Enter your name");
+// var val1 = val.toUpperCase();
+// alert(val1);
+
+// var val=prompt("Enter your name");
+// var val1 = val.toLowerCase();
+// alert(val1);
+
+
+// var mobile = prompt("Enter mobile name");
+// var mobile1 = mobile.length;
+// document.write(mobile1);
+
+
+// var last = prompt("enter your name");
+// var last1 = last.lastIndexOf();
+// alert(last1);
+
+
+// var country = "Pakistani";
+// var result = country.lastindexof("Pakistani");
+// document.write(result);
+
+// var s = prompt("enter your  name");
+// if(s == "@#$%^&*()"){
+//        alert("Please enter alphabats");
+// }
+// else
+// {
+//     alert("ok");
+// }
